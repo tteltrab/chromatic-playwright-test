1552b35077c75e43921e3a664808888e19c27115
Nick Bartlett <nbartlett7@gmail.com>
2024-05-20 20:17:06 -0400

    add chromatic, initial run with logs

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@chromatic-com/playwright";
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
